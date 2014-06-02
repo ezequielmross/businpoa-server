eb74a39b059f1a7444ac58d5aada0e9f03bbe3ae
Ezequiel <ezequielmross@gmail.com>
2014-06-02 14:53:56 -0300

    Update server

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var request = require('request'),
 // 
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
     next();
 });
 
@@ -22,10 +22,32 @@ app.get("/ping", function Ping(req, res) {
 });
 
 
-// list bus
+// list bus  
 app.get("/list/bus", function ListBus(req, res) {
-    
+    console.log("teste");
     request("http://www.poatransporte.com.br/php/facades/process.php?a=nc&p=%&t=o", function (error, response, body) {
+        var parsedValue = JSON.parse(body);
+        res.json(parsedValue);
+    });
+    
+});
+
+app.get("/list/lotacao", function ListBus(req, res) {
+    
+    request("http://www.poatransporte.com.br/php/facades/process.php?a=nc&p=%&t=l", function (error, response, body) {
+        var parsedValue = JSON.parse(body);
+        res.json(parsedValue);
+    });
+    
+});
+
+app.get("/itinerario/:id", function ListBus(req, res) {
+    
+    request("http://www.poatransporte.com.br/php/facades/process.php?a=il&p="+req.params.id, function (error, response, body) {
+		if(error){ 
+			
+			return;
+		}
         var parsedValue = JSON.parse(body);
         res.json(parsedValue);
     });
